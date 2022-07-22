a21aa819aa6e29ecb01e466b721c2e9fb9ed9e27
Critix <deakrittika@gmail.com>
2022-07-22 17:40:52 +0200

    Merge

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,28 +4,73 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public Rigidbody2D rb;
+    public float moveSpeed;
+    private Rigidbody2D rb;
+    public float jumpForce;
+    private bool facingRight = true;
 
-    Vector2 movement;
+    private float moveInput;
+    private bool jumpInput;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private int extraJumps;
+    public int extraJumpValue;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        extraJumps = extraJumpValue;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame, for registering Input
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
         
+        
+
+        moveInput = Input.GetAxis("Horizontal");
+        Debug.Log(moveInput);
+        if(facingRight && moveInput < -0.01f)
+        {
+            Flip();
+        } else if (!facingRight && moveInput > 0.01f) {
+            Flip(); 
+        }
+
+        if (isGrounded)
+        {
+            extraJumps = extraJumpValue;
+        }
     }
 
     // Update for Physics, called 50 times a second
     void FixedUpdate()
     {
         //Movement
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+        //Not sure if in Update or FixedUpdate
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        //Jump
+        jumpInput = Input.GetKeyDown(KeyCode.Space);
+        if (jumpInput && extraJumps > 0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        } 
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
     }
 }
