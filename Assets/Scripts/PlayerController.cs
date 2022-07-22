08c22214574605df6a14dca4ea826ee86e588b2d
Pat Colyn <patcolyn@gmail.com>
2022-07-22 16:21:18 +0100

    Player_Finished

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public CharacterController2D controller;
+    public bool primary = false;
 
     public float runSpeed = 40f;
     bool jump = false;
@@ -12,6 +13,18 @@ public class PlayerController : MonoBehaviour
 
     public Color PlayerColor;
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>()) {
+
+            PlayerController other = collision.gameObject.GetComponent<PlayerController>();
+
+            if (other.PlayerColor == PlayerColor)
+            {
+                //Destroy(transform.gameObject);
+            }
+        }
+    }
 
     private void Start()
     {
