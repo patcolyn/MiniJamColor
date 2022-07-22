79b88e8a2d4d77053d70d7b469b1c3214496cc00
Critix <deakrittika@gmail.com>
2022-07-23 00:46:26 +0200

    Auto stash before cherry pick of "Jump Fix"

--- Assets/Scripts/ExitLevel.cs
+++ Assets/Scripts/ExitLevel.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 public class ExitLevel : MonoBehaviour
 {
-    private GameObject[] objs;
+    private GameObject[] endLevelTriggers;
+    private GameObject[] menu;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,14 +22,33 @@ public class ExitLevel : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        objs = GameObject.FindGameObjectsWithTag("EndTriggers");
-        foreach (GameObject endTrigger in objs)
+        endLevelTriggers = GameObject.FindGameObjectsWithTag("EndTriggers");
+        foreach (GameObject endTrigger in endLevelTriggers)
         {
             if (!endTrigger.GetComponent<EndLevelTrigger>().getIsActivated()) {
                 return;
             }
         }
         Debug.Log("ALL ACTIVE");
-        SceneManager.LoadScene("EndLevelScreen"); //Load end level screen
+        activateMenu();
+    }
+
+    private void activateMenu()
+    {
+        menu = GameObject.FindGameObjectsWithTag("Menu");
+        foreach(GameObject menuElement in menu)
+        {
+            if(menuElement.name == "Canvas")
+            {
+                menuElement.transform.position = Vector3.zero;
+                menuElement.GetComponent<Canvas>().enabled = true;
+                menuElement.GetComponent<CanvasScaler>().enabled = true;
+                menuElement.GetComponent<GraphicRaycaster>().enabled = true;
+            } else
+            {
+                menuElement.GetComponent<EventSystem>().enabled = true;
+                menuElement.GetComponent<StandaloneInputModule>().enabled = true;
+            }
+        }
     }
 }
